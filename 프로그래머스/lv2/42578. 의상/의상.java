a3af50c2c3c689c541538fb8d62ebf1f61708dfa
yoojaeyoon <110767749+yoojaeyoonGit@users.noreply.github.com>
2023-09-14 00:07:57 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -2,21 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> hash = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++) {
-            if (hash.containsKey(clothes[i][1])) {
-                hash.put(clothes[i][1], hash.get(clothes[i][1]) + 1);
-            }
-            else {
-                hash.put(clothes[i][1], 1);
-            }
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i ++ ) {
+            hm.put(clothes[i][1], 0);
         }
+
+        for (int i = 0; i < clothes.length; i ++ ) {
+            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+
         int answer = 1;
 
-        Iterator<Integer> valueIterator = hash.values().iterator();
-        while (valueIterator.hasNext()) {
-            answer *= valueIterator.next() + 1;
+        Iterator<Integer> iterator = hm.values().iterator();
+        while (iterator.hasNext()) {
+            answer *= iterator.next() + 1;
         }
+
+
         return answer - 1;
     }
 }
